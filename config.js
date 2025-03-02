ca444c42426a2f91778ce6b0b0c613e09213ce00
Dilshan544 <ashinsadilshan64@gmail.com>
2025-03-02 11:37:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID, "PRABATH-MD~dic0CJrJ#cK-Mtq9oudi4XhwzYbCCE_PkP41JNEu1siI6yzyg8fo"
 ALIVE_IMG: process.env.ALIVE_IMG || "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.deviantart.com%2Fzephetron%2Fart%2FAnime-Boy-884409925&psig=AOvVaw1Txww46DGFfV4qqEJaMR0y&ust=1740812813340000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCIj0mJ_n5YsDFQAAAAAdAAAAABAJ",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello👋 I am 🧑‍💻 ▏DILSHAN_MD ▏🧑‍💻 I am alive now ||",
 }:
